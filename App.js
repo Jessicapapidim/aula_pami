75c197de05f6179de7d6ceb0f93cb417c6e5e1ab
Jessicapapidim <124743212+Jessicapapidim@users.noreply.github.com>
2023-06-15 20:34:44 -0300

    15062023

--- App.js
+++ App.js
@@ -6,15 +6,15 @@ let corAPP = '#fff '
 // import Apostila03 from './components/apostila_003';
 // import Apostila03A from './components/apostila_apo03avancado';
 //import Apostila04 from './components/apostila_004';
-//import Apostila_004_ativ from './components/apostila_004_ativ';
-//import Apostila_05 from './components/apostila_05';
+import Apostila_004_ativ from './components/apostila_004_ativ';
+import Apostila_05 from './components/apostila_05';
 import Apostila_06 from './components/apostila_06';
 
 let corApp = '#000';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Apostila_06 />
+      <Apostila_004_ativ />
       <StatusBar style={corAPP === '#000' ? "light" : "dark"} />
     </View>
   );
